26a18fc240e97915320cdf504abccd6cbc4be5dc
fikriff <fikri220296@gmail.com>
2019-02-25 14:46:32 +0700

    Add AudioManager & GameData

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : Singleton<AudioManager>
+{
+    // Start is called before the first frame update
+    AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource.volume = GameData.Instance.GetSFXVolume();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlaySFXClip(AudioClip clip){
+        audioSource.PlayOneShot(clip);
+    }
+}
---
+++ Assets/Scripts/GameData.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameData : Singleton<GameData>
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void SetSFXVolume(float volume){
+        PlayerPrefs.SetFloat("SFXVolume", volume);   
+        Debug.Log(GetSFXVolume());
+    }
+
+    public float GetSFXVolume(){
+        return PlayerPrefs.GetFloat("SFXVolume",1);
+    }
+}
