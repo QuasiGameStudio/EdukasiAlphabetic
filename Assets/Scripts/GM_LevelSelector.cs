fa392e7d0ef87a0a9c5d4b751b5ca9b7028a5e42
fikriff <fikri220296@gmail.com>
2019-02-28 12:18:34 +0700

    Menambahkan Animasi Untuk Menu selector

---
+++ Assets/Scripts/GM_LevelSelector.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GM_LevelSelector : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField]
+    private GameObject[] Icons;
+
+    void Start()
+    {
+        StartCoroutine(PopUpIcon());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator PopUpIcon()
+    {
+        for (int i = 0; i<Icons.Length; i++){
+            Icons[i].SetActive(true);
+            Icons[i].GetComponent<Animator>().SetTrigger("Start");
+            yield return new WaitForSeconds(2);
+        }
+    }
+}
---
+++ Assets/Scripts/GM_StudySelector.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GM_StudySelector : MonoBehaviour
+{
+    // Start is called before the first frame update    
+    [SerializeField]
+    private GameObject[] Icons;
+
+    void Start()
+    {
+        StartCoroutine(PopUpIcon());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator PopUpIcon()
+    {
+        for (int i = 0; i<Icons.Length; i++){
+            Icons[i].SetActive(true);
+            Icons[i].GetComponent<Animator>().SetTrigger("Start");
+            yield return new WaitForSeconds(1);
+        }
+    }
+}
